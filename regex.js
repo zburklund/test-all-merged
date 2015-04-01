62b4d6f6f8ecca22eb6782283956bfc488e0ba94
Ben Margevicius <bdm4@case.edu>
2015-04-01 13:03:47 -0400

    Added console test
    Instead of using a button to test the id, i'll just use the console to
    output the test results.

--- regex.js
+++ regex.js
@@ -15,15 +15,43 @@ abc0
 abc12345
 */
 
-var caseidregex = "/^[a-zA-Z]{3}([1-9]\d{0,3})?$/";
+/*
+Case ID Regular Expression 
+4/1/2015 bdm4 
+v0.1
 
-$(function(){
-	$("#TestBtn").click(function(){
-		var totest = $("#CaseIdTxt").html();
-		if(caseidregex.match(totest))
-			$("#ResultsTxt").html(totest + "matches the case id pattern");
-		else
-			$("#ResultsTxt").html(totest + "does not match the case id pattern");		
-	});		
+CaseUserIDs can be
+
+abc
+abc1
+abc12
+abc123
+abc1234
+
+But not
+abc0 
+abc12345
+*/
+
+$(function () {
+    var caseidregex = /^[a-zA-Z]{3}([1-9]\d{0,3})?$/;
+    var abc = "abc",    
+        abc1 = "abc1",
+        abc12 = "abc12",
+        abc123 = "abc123",
+        abc1234 = "abc1234",
+        abc0 = "abc0",
+        abc12345 = "abc12345";
+
+    
+	console.log("Testing '" + abc + "' : " + caseidregex.test(abc));
+	console.log("Testing '" + abc1 + "' : " + caseidregex.test(abc1));
+	console.log("Testing '" + abc12 + "' : " + caseidregex.test(abc12));
+	console.log("Testing '" + abc123 + "' : " + caseidregex.test(abc123));
+	console.log("Testing '" + abc1234 + "' : " + caseidregex.test(abc1234));
+	console.log("Testing '" + abc0 + "' : " + caseidregex.test(abc0));
+	console.log("Testing '" + abc12345 + "' : " + caseidregex.test(abc12345));
 })
+
+
 
