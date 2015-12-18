659d6b6c930c878e893d3442ec6c44e700e2f82f
Ben Margevicius <bdm4@case.edu>
2015-12-18 17:59:26 -0500

    Nightly
    Now Getting a valid XML response form CAS :+1:

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.IO;
+using System.Net;
+using System.Xml.Linq;
 
 namespace CASP
 {
@@ -22,25 +25,47 @@ namespace CASP
         CAS Protocol Versions Supported: 1, 2 and 3
         **/
 
+        const string LOGIN_URL = "https://login.case.edu/cas/login";
         const string SERVICE_VALIDATE_URL = "https://login.case.edu/cas/p3/serviceValidate";
+
 
         protected void Page_Load(object sender, EventArgs e)
         {
             string ticket = Request.QueryString["ticket"];
             string service = Request.Url.GetLeftPart(UriPartial.Path);
 
-            //If there is no ticket in the query string.
+            //If there is no ticket in the query string. We need to login to SSO
             if (String.IsNullOrEmpty(ticket))
             {
-                string redirect = SERVICE_VALIDATE_URL + "?ticket=" + ticket + "&service=" + service;
-                Response.Redirect(redirect);
+                string login = LOGIN_URL + "?service=" + service;
+                Response.Redirect(login);
                 return;
             }
 
             //Back from SSO there should be a ticket to validate.
+            string validateurl = SERVICE_VALIDATE_URL + "?ticket=" + ticket + "&service=" + service;
+            using (StreamReader reader = new StreamReader(new WebClient().OpenRead(validateurl)))
+            {                
+                //string validResponse = reader.ReadToEnd(); //just for debugging. 
+                /* Valid Service Response
+                 * <cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>
+                        <cas:authenticationSuccess>
+                            <cas:user>abc12</cas:user>
+                            <cas:attributes>
+                                <cas:user>abc12</cas:user>
+                            </cas:attributes>
+                         </cas:authenticationSuccess>
+                    </cas:serviceResponse> 
+                */
 
+                XDocument doc = XDocument.Load(reader);
+              
+                
+                
+            }
+            
 
-            bool isAuth = ActiveDirectory.isUserAuthorized("bdm4", "nurs-dept-it");
+            //bool isAuth = ActiveDirectory.isUserAuthorized("bdm4", "nurs-dept-it");
         }
     }
 
